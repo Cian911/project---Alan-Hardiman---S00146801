d97e00184b9c9a6ef7fd33a48e1f44c9f84e23f7
Hman1994 <al-hardiman@hotmail.com>
2016-03-15 22:42:29 +0000

    commits

---
+++ project/Models/Countries.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace project.Models
+{
+      public class Countries
+      {
+            public int ID { get; set; }
+            [Display(Name  = "Country")]
+            public string NameOfCountry { get; set; }
+            [Display(Name = "Capital City")]
+            public string CapitalCity { get; set; }
+            [Display(Name = "Currency")]
+            public string Currency { get; set; }
+            [Display(Name = "Language")]
+            public string Language { get; set; }
+       }
+
+    public class CountriesDBContext : DbContext
+    {
+        public DbSet<Countries> Country { get; set; }
+    }
+}
+
+    
